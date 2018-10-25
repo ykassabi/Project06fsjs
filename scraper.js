75431d8ae2cb01014084178f8f5e3ec1a36241e4
YKASSABI <youssef@devmyk.com>
2018-10-25 11:34:41 +0100

    added module to convert json to csv opimisation of the date to string for universal compatibility

--- scraper.js
+++ scraper.js
@@ -1,6 +1,7 @@
 const request = require('request');
 const cheerio = require('cheerio');
-const fs = require('fs')
+const fs = require('fs');
+const Json2csvParser = require('json2csv').Parser;
 
 
 
@@ -13,10 +14,10 @@ if (!fs.existsSync(dir)) {
 ///////////////////////////////////////////////
 //get all the links of the products(shirts) and store it on arr
 function grabingURLsShirtAsync() {
-// variable  for the target website. 
-const rootURL = 'http://www.shirts4mike.com/';
-const productShirtsURL = rootURL + 'shirts.php';
-    
+    // variable  for the target website. 
+    const rootURL = 'http://www.shirts4mike.com/';
+    const productShirtsURL = rootURL + 'shirts.php';
+
     return new Promise(function (resolve, reject) {
         request(productShirtsURL, function (error, response, html) {
 
@@ -47,21 +48,22 @@ const productShirtsURL = rootURL + 'shirts.php';
 function shirtsDetailsAsync() { //to access detail on each shirt page.
     return grabingURLsShirtAsync().then(function (arrOfURLShirt) {
 
-//will grabe and store the details; title  price url and urlimage
+            //will grabe and store the details; title  price url and urlimage
             const promises = arrOfURLShirt.map(function (tshirtURL) {
 
                 return new Promise(function (resolve, reject) {
 
                     request(tshirtURL, function (error, response, html) {
                         if (!error && response.statusCode === 200) {
+                            let fullDateItem = new Date().toISOString();
                             let $ = cheerio.load(html); //cheerio load the html code 
                             //grabing the details; title , price, URL, imageURL
                             let productDetails = {
-                                title: $('.shirt-details h1').text().slice(4),
-                                price: $('.shirt-details h1 span').text(),
-                                imageUrl: $('.shirt-picture span img').attr('src'),
-                                url: tshirtURL,
-                                time: new Date()
+                                Title: $('.shirt-details h1').text().slice(4),
+                                Price: $('.shirt-details h1 span').text(),
+                                imageURL: $('.shirt-picture span img').attr('src'),
+                                URL: tshirtURL,
+                                Time: fullDateItem
                             };
                             resolve(productDetails)
                         } else {
@@ -73,41 +75,30 @@ function shirtsDetailsAsync() { //to access detail on each shirt page.
             return Promise.all(promises); // delivering all promises at one
         })
         .then(function (data) {
-// transforming from JSON data format to csv, after this 
-            let jsondata = JSON.stringify(data);
-            let csvData = ConvertToCSV(jsondata);// convertToCSV is just bellow
-            return csvData;
+            const fields = ["Title", "Price", "imageURL", "URL", "Time"];
+            const json2csvParser = new Json2csvParser({
+                fields
+            });
+
+            const csv = json2csvParser.parse(data);
+            console.log(csv)
+            return csv;
         })
         .catch(function (err) {
             return Promise.reject(err);
         });
-}
-//////////////////////////////////////////////////
-//
-function ConvertToCSV(objArr) {
-    var array = typeof objArr != 'object' ? JSON.parse(objArr) : objArr;
-    var inputData = `Title, Color, Price, ImageURL, URL, and Time \r\n`;
-
-    for (var i = 0; i < array.length; i++) {
-        var line = '';
-        for (var index in array[i]) {
-            if (line != '') line += ','
-
-            line += array[i][index];
-        }
-        inputData += line + '\r\n';
-    }
-    return inputData;
 }
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 // 
 function scraperFunction() {
+    let fullDate = new Date().toISOString();
+    let shortDate = fullDate.slice(0, 10);
     shirtsDetailsAsync()
         .then(data => {
             // writing error csv file to data folder
-            fs.writeFile(`./data/${new Date().toLocaleDateString()}.csv`, data, 'utf-8', err => {
+            fs.writeFile(`./data/${shortDate}.csv`, data, 'utf-8', err => {
                 if (err) {
                     console.log(err.message);
                 } else {
@@ -116,8 +107,8 @@ function scraperFunction() {
             });
         })
         .catch(err => {
-            console.log(`${new Date()}!! Oh !! WE HAVE AN ERROR ${err}`);
-            let errData = `${new Date()} WE HAVE AN ERROR ${err} \r\n`;
+            let errData = `${fullDate} WE HAVE AN ERROR ${err} \r\n`;
+            console.log(errData);
             //writing the error to log file, 
             fs.appendFile('scraper-error.log', errData, 'utf8', (err) => {
                 if (err) throw err;
@@ -127,12 +118,13 @@ function scraperFunction() {
 }
 // //////////////////////////////////////////////////////////////////////
 //  Immediate invoking the function 
-setImmediate(()=> {
-scraperFunction();
+setImmediate(() => {
+    scraperFunction();
 })
 ///////////////////////////////////////////////
 // one a day incoking the function
 setInterval(() => {
     scraperFunction();
-}, 86400000) //one day;
+}, 20000) //one day;
+// }, 86400000) //one day;
 ///////////////////////////////////////////////
